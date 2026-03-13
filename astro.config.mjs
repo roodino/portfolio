6fdcfbedf168ff0a1b71ec882aefac671ec994af
Marat Sarvarov <shtirlizc@gmail.com>
2026-03-13 06:37:34 +0500

    default tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig, fontProviders} from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [{
+        provider: fontProviders.local(),
+        name: "SF-Pro",
+        cssVariable: "--font-sf-pro",
+        options: {
+            variants: [{
+                src: ['./src/assets/fonts/SF-Pro.woff2'],
+                weight: '100 900',
+                style: 'normal'
+            }]
+        }
+    }]
+});
