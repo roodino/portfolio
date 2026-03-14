255e80ed2bc57620d264d61a871673685516bcbb
Marat Sarvarov <shtirlizc@gmail.com>
2026-03-14 21:48:10 +0500

    meta tags, fix styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import {defineConfig, fontProviders} from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://gazov.pro',
     output: 'static',
+    integrations: [sitemap()],
     redirects: {
         "/": "/work",
         "/404": "/work",
